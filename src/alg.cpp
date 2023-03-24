ef733997410855e3267a243824875cbbba20f227
JuliaKorshunova <116427627+JuliaKorshunova@users.noreply.github.com>
2023-03-24 22:36:59 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,7 +1,7 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
     int a = 0;
-    for (int i = 0; i < len; i++) {
+    for (int i = 0; i < len-1; i++) {
         for (int j = i+1; j < len; j++) {
             if (*(arr + i) + *(arr + j) == value) {
                 a++;
@@ -12,8 +12,8 @@ int countPairs1(int *arr, int len, int value) {
 }
 int countPairs2(int *arr, int len, int value) {
     int a = 0;
-    for (int i = 0; i < len; i++) {
-        for (int j = len; i < j; j--) {
+    for (int i = 0; i < len-1; i++) {
+        for (int j = len-1; i < j; j--) {
             if (*(arr + i) + *(arr + j) == value) {
                 a++;
             }
